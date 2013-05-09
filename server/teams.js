66e6d4ce1db4bd3b683a5d05f9e2d259d01ab1ac
Vadim Zaytsev <zaic101@gmail.com>
2013-05-10 01:19:59 +0700

    nyaaa~ ^^

--- server/teams.js
+++ server/teams.js
@@ -1,11 +1,7 @@
 Meteor.publish("teams", function() {
-<<<<<<< HEAD
     return Meteor.users.find({}, {fields: {'username': 1, 
                                            'profile.from_novosib': 1,
                                            'profile.country': 1,
                                            'profile.score': 1
                                           } });
-=======
-    return Meteor.users.find({}, {fields: {username: 1, 'profile.country': 1, 'profile.from_novosib': 1} });
->>>>>>> fc2b1cc72c2c386f15f65f8967d24caefa43fa0a
 });
