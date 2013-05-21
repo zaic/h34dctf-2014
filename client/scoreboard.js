704883391138e3779fb7d0d300d1cd79d3c6c21b
Vadim Zaytsev <zaic101@gmail.com>
2013-05-22 01:37:32 +0700

    Scoreboard.

--- client/scoreboard.js
+++ client/scoreboard.js
@@ -1,6 +1,6 @@
 Template.scoreboard.teams = function() {
     var accepted_nsk = Session.equals('scoreboard_teams', 'nsk') ? [true] : [true, false];
-    var teams = Meteor.users.find({'profile.score': {$gt: 0}, 'profile.from_novosib': {$in: accepted_nsk}}, {sort: [['profile.score', 'desc'], ['profile.last_success', 'asc']]});
+    var teams = Meteor.users.find({'profile.from_novosib': {$in: accepted_nsk}}, {sort: [['profile.score', 'desc'], ['profile.last_success', 'asc']]});
 
     var last_score   = -1;
     var last_time    = -1;
@@ -28,6 +28,10 @@ Template.scoreboard.teams = function() {
             last_success: t
         };
     });
+};
+
+Template.scoreboard.isNskOnly = function() {
+    return Session.equals('scoreboard_teams', 'nsk');
 };
 
 Template.scoreboard.events({
