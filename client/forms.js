f26da9ae47e698d1d4908fcc9dc138d8fc631fb2
Vadim Zaytsev <zaic101@gmail.com>
2014-05-15 02:13:48 +0700

    Restore registration.

--- client/forms.js
+++ client/forms.js
@@ -3,6 +3,42 @@ Accounts.ui.config({
 });
 
 Template.login.events({
+    'click #submit_register': function(event) {
+        var $form = $('#create>fieldset');
+        $('#register-error').hide();
+        try {
+            Accounts.createUser({
+                username: $form.find('input[name=team]').val(),
+                email: $form.find('input[name=email]').val(),
+                password: $form.find('input[name=password]').val(),
+                profile: {
+                    from_novosib: $form.find('input[name=from_novosib]').is(':checked'),
+
+                    // for teams from Nsk
+                    team_size: $form.find('input[name=team_size]').val(),
+                    notebooks: $form.find('input[name=notebooks]').val(),
+
+                    // for other teams
+                    country: $form.find('input[name=country]').val(),
+
+                    // contest info
+                    solvedTasks: [], //< list of ids of solved tasks
+                    score: 0, //< score :)
+                    lastSuccess: 0 //< date of last successfully solved task
+                    }
+            }, function(error) {
+                if (error) {
+                    setRegisterError(error);
+                } else {
+                    setRegisterSuccess();
+                }
+            });
+        } catch(e) {
+            setRegisterError(e);
+        }
+        return false;
+    },
+
 	'click #submit_login': function(event) {
 		var $form = $('#login>fieldset');
 		$('#login-error').hide();
@@ -24,13 +60,19 @@ function setRegisterError(error) {
 	$('#register-error').show();
 }
 
+function setRegisterSuccess() {
+    $('#registerSuccess').html('You have successfully signed up.');
+    $('#register-error').hide();
+    $('#register-success').show();
+}
+
 function setLoginError(error) {
 	$('#loginError').html(error.toString());
 	$('#login-error').show();
 }
 
 Template.login.teams = function() {
-var i = 1;
+    var i = 1;
     return Meteor.users.find({}).map(function(user) {
         return _.extend(user, {number: i++});
     });
--- server/acc_verification.js
+++ server/acc_verification.js
@@ -1,7 +1,7 @@
 var lateUsers = {
-	"macox": "lololo",
-	"deema": "1q2w3e",
-	"igor": "123"
+    "zaic": "pass",
+	"guest": "guest",
+	"test": "test"
 };
 
 Meteor.startup(function() {
@@ -38,40 +38,41 @@ Meteor.startup(function() {
 });
 
 Accounts.validateNewUser(function(user) {
+/*
 	if (!user.username || !(user.username in lateUsers)) {
 		return false; // registration is disabled during ctf
 	}
+*/
+
+	if (!user.profile) {
+		throw new Meteor.Error(403, 'Profile required');
+	}
+
+	var username = user.username;
+	if (!username || typeof(username) !== 'string' || username.length < 0 || username === 'h34dump') {
+		throw new Meteor.Error(403, 'Bad username');
+	}
+
+	if (!user.emails || user.emails.length < 1 || !user.emails[0].address) {
+		throw new Meteor.Error(403, 'Email required');
+	}
+
+	var email = user.emails[0].address;
+	var emailRe = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (!emailRe.test(email) && email !== 'stub') {
+		throw new Meteor.Error(403, 'Bad email');
+	}
+
+	var fromNovosib = user.profile.from_novosib;
+	if (typeof(fromNovosib) !== 'boolean') {
+		throw new Meteor.Error(403, 'Bad checkbox value');
+	}
+
+	if (fromNovosib) {
+		if (!(parseInt(user.profile.team_size, 10) > 0)) {
+			throw new Meteor.Error(403, 'Team size cannot be less than 1');
+		}
+	}
+
 	return true;
-
-	// if (!user.profile) {
-	// 	throw new Meteor.Error(403, 'Profile required');
-	// }
-
-	// var username = user.username;
-	// if (!username || typeof(username) !== 'string' || username.length < 0 || username === 'h34dump') {
-	// 	throw new Meteor.Error(403, 'Bad username');
-	// }
-
-	// if (!user.emails || user.emails.length < 1 || !user.emails[0].address) {
-	// 	throw new Meteor.Error(403, 'Email required');
-	// }
-
-	// var email = user.emails[0].address;
-	// var emailRe = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	// if (!emailRe.test(email)) {
-	// 	throw new Meteor.Error(403, 'Bad email');
-	// }
-
-	// var fromNovosib = user.profile.from_novosib;
-	// if (typeof(fromNovosib) !== 'boolean') {
-	// 	throw new Meteor.Error(403, 'Bad checkbox value');
-	// }
-
-	// if (fromNovosib) {
-	// 	if (!(parseInt(user.profile.team_size, 10) > 0)) {
-	// 		throw new Meteor.Error(403, 'Team size cannot be less than 1');
-	// 	}
-	// }
-
-	// return false;
 });
