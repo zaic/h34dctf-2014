6c1a2d6eb57d1c72333a47fc02a26a6ab7c5b489
Vadim Zaytsev <zaic101@gmail.com>
2014-05-21 01:06:40 +0700

    Update tasks for current year.

--- server/tasks.js
+++ server/tasks.js
@@ -2,6 +2,59 @@
 // note that names are expected to be unique and contain no whitespaces
 tasks = [
 	{
+		name: 'lvm',
+		category: 'Admin',
+		description: '<a href="bfc3f01a37bbb2dce6f611bc71cb35ef">Task</a>.',
+		value: 100,
+		available: true,
+		solved: 0,
+		hints: [],
+
+		checkFlag: function(flag) {
+			return (flag === 'flag_is_zfs_better_lvm' || flag === 'zfs_better_lvm');
+		}
+	},
+    {
+		name: 'passwd',
+		category: 'Binary',
+		description: 'This tool ask me for a password! Hack <a href="a6fb82b4cf21ef7a51a3e606127a93e7">it</a>!.',
+		value: 100,
+		available: true,
+		solved: 0,
+		hints: [],
+
+		checkFlag: function(flag) {
+			return (flag === 'flag_is_wonderful_binary_task' || flag === 'wonderful_binary_task');
+		}
+	},
+    {
+		name: 'love',
+		category: 'Binary',
+		description: 'All you need is love. And some star wars. <a href="04e08f6b9fa76abdfdb5fb2baee58dc2">Here</a>.',
+		value: 300,
+		available: true,
+		solved: 0,
+		hints: [],
+
+		checkFlag: function(flag) {
+			return (flag === '=======M@ke_1oVe_n0t_VV@r=======');
+		}
+	},
+    {
+		name: 'windows',
+        category: 'Stegano',
+		description: 'Windows 8 is amazing, <a href="0deb22024281d7d7b1aec9829ed2f598">right</a>?',
+		value: 100,
+		available: true,
+		solved: 0,
+		hints: [],
+
+		checkFlag: function(flag) {
+			return (flag === 'flag_is_least_significant_bit' || flag === 'least_significant_bit');
+		}
+	},
+
+	{
 		name: 'cow_level',
 		category: 'Stegano',
 		description: 'Secret <a href="79805faac89d408d0c4c73b465cac3ce" target="_blank">cow level</a>',
@@ -25,255 +78,6 @@ tasks = [
 
 		checkFlag: function(flag) {
 			return (flag === 'd0eS_th3_m4n_makE_t4e_suit');
-		}
-	},
-	{
-		name: 'mount',
-		category: 'Forensics',
-		description: 'I store my flag in this <a href="https://www.dropbox.com/s/xeu61pija9t9mcn/for-50-08a4743c2e4a99d20af95f8c642a4697" target="_blank">file</a>, please find it.',
-		value: 50,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === 'This_was_S0000_simPL3!');
-		}
-	},
-	{
-		name: 'excel',
-		category: 'Forensics',
-		description: 'I got this <a href="https://www.dropbox.com/s/v4yr70wsxfph6zx/for-50-2-9d77207b9490d5e9d43197be98d8608b" target="_blank">file</a> from my enemy.</br> Can you check it for viruses and other evil staff?',
-		value: 50,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === '3vil_maKKKr0s_I5_v3333rY_3vIl');
-		}
-	},
-	{
-		name: 'excel_returns',
-		category: 'Forensics',
-		description: 'Wow, I\'ve seen a person editing this <a href="https://www.dropbox.com/s/r8vrtre3vr1a4jo/for-100-05eaeaada3940223fc2a56d592e3df2c" target="_blank">file</a>,<br> but it is kind of empty now. Can you check it out?',
-		value: 100,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === '3xc3l_|s_s0_serious');
-		}
-	},
-	{
-		name: 'forest',
-		category: 'Forensics',
-		description: 'Please, find my key in this <a href="https://www.dropbox.com/s/8x12p8m11zg3yri/for-200-55db916996935e45ffe4f2459ddeeea2" target="_blank">forest</a>! PS: 0 - left, 1 - right.',
-		value: 200,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === 'deEp_s3aRCh-m4St#r');
-		}
-	},
-	{
-		name: 'order',
-		category: 'Forensics',
-		description: 'Hi! I want to replace something in my notebook, but I forgot order of these <a href="https://www.dropbox.com/s/oiq9gjian357pjm/for-300-af5d6f419893659a042229419cbc470f" target="_blank">photos</a>,</br> could you help me?',
-		value: 300,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === 'pl34sE_don_t_use_scr3wdr|v3r_i_do_not_d3s3rvE_it');
-		}
-	},
-	{
-		name: 'resurrection',
-		category: 'Forensics',
-		description: 'Our <a href="https://www.dropbox.com/s/xidi9achphm2s1j/for-400-f5918a911f23118812e5f8d9fc67f366.rar" target="_blank">database</a> was badly damaged by unknown hackers.</br> It looks like they are playing with us, because they haven\'t deleted all files</br> and encrypted some files names of the rest.</br> See if you can find anything that wasn\'t corrupted.<br> Send the flag in uppercase letters without any whitespaces.',
-		value: 400,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return(flag === 'NOTHINGTODOHERE11');
-		}
-	},
-	{
-		name: 'bindata',
-		category: 'Crypto',
-		description:'Get a <a href="https://www.dropbox.com/s/r3iu5ilhwbg42hp/cry-100-40ca18537878a487df2d8adcac34b82c" target="_blank">flag</a>.',
-		value: 100,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === 'x0r_XoR_xO_x0');
-		}
-	},
-	{
-		name: 'templars',
-		category: 'Crypto',
-		description: 'We got <a href="https://www.dropbox.com/s/8zqx1wkchyugmek/cry100-844e2f93c36a0ae2c92038b1d5333665.JPG" target="_blank">this</a> ' +
-						'from Knights Templar hideout. What are they planning?',
-		value: 200,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag.toUpperCase() === 'HAVEYOUPLAYEDFOREZIO');
-		}
-	},
-	{
-		name: 'moveit',
-		category: 'Crypto',
-		description: 'Classical cipher like l33t ' +
-				'<a href="https://www.dropbox.com/sh/2wll8vvwy4hf23c/KeNdXRDPUc/crypto200_encrypted.txt" target="_blank">task</a>',
-		value: 150,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === 'cryptool_0r_manu4l');
-		}
-	},
-	{
-		name: 'kryptozz',
-		category: 'Crypto',
-		description: 'Give sequence of digits in geolocation from <a href="https://www.dropbox.com/s/rjf7kmzgy5zw9jq/crypto50.jpg" target="_blank">part2</a> of plaintext.',
-		value: 50,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === '38576577844');
-		}
-	},
-	{
-		name: 'keyasker',
-		category: 'Binary',
-		description: 'Get a key, pleeeeeeease! <a href="/aa2b3d769140c440e44fcfeba3757081" target="_blank">file</a>',
-		value: 100,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === '69ccc0dd616f12d151525fc9f753ec9f');
-		}
-	},
-	{
-		name: 'greeter1',
-		category: 'Binary',
-		description: 'This service just says hello to everyone! I hate it! Do something with it!<br>ssh user@ctf.h34dump.com -p22200<br>pass: user',
-		value: 200,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === '0v3rfl0wn_buff3r_1s_s0_cu7e');
-		}
-	},
-	{
-		name: 'greeter2',
-		category: 'Binary',
-		description: 'Another damned service!<br>ssh user@ctf.h34dump.com -p22201<br>pass: user',
-		value: 300,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === 'c4r3ful_w1th_f0rm4t');
-		}
-	},
-	{
-		name: 'misa_encoding',
-		category: 'Crypto',
-		description: 'misa sent me this <a href="/bd72715c3eeb690fee8cdd4642d61c1e" target="_blank">letter</a> recently. Does it mean anything?<br>(flag is md5(decoded_letter))',
-		value: 100,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === '474e9b1cf54cd2581bbdabe111fff13c');
-		}
-	},
-	{
-		name: 'qrcode',
-		category: 'Stegano',
-		description: 'No, it\'s not what you think. Get a <a href="https://www.dropbox.com/s/9j50suo5v9jrsji/qrcode.png" target="_blank">flag</a>',
-		value: 100,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === 'U_R_r3333333333333Ly_$T360_m4St4');
-		}
-	},
-	{
-		name: 'h4x0r_library',
-		category: 'Web',
-		description: '<a href=\'/web1/\'>link</a>',
-		value: 200,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === 'l0c4l_c0d3_3x3cut10n_1s_f1n3');
-		}
-	},
-	{
-		name: 'h4x0rs_food',
-		category: 'Web',
-		description: '<a href=\'/web0/\'>link</a>',
-		value: 50,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === 'c00k13s_4r3_t4sty_4nd_h34lthy');
-		}
-	},
-	{
-		name: 'gnp',
-		category: 'Stegano',
-		description: 'Another stupid stegano task. Here, <a href="/3ba2b0efa06a6423c6dd513259bfd900" target="_blank">get it</a>.',
-		value: 300,
-		available: true,
-		solved: 0,
-		hints: [],
-
-		checkFlag: function(flag) {
-			return (flag === 'f1r5T_l4dy_0f_t3h_1nT3rn3t');
-		}
-	},
-	{
-		name: 'virtual',
-		category: 'Admin',
-		description: 'Get a flag from <a href="https://www.dropbox.com/s/j1s64xszrxqu75f/admin.zip" target="_blank">this</a>.',
-		value: 300,
-		available: true,
-		solved: 0,
-		hints: [],
-		
-		checkFlag: function(flag) {
-			return (flag === 'L1ttL3_th1ngz_4ff3ct_l1ttL3_m1ndz');
 		}
 	}
 ];
